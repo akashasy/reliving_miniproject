717a777cceee78b5efaf87b7f1647478e4493a56
akashasy <akashasy53@gmail.com>
2022-05-04 22:23:44 +0530

    Some changes

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,9 @@
 import  express  from 'express';
 import { getPosts ,createPost } from '../controllers/posts.js';
 const router=express.Router();
+
 router.get('/',getPosts);
+//Recived from posts.js
 router.post('/',createPost);
 
 export default router;

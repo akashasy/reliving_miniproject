53f37174b00cd275f0fbf94230d8effc7e4f9368
akashasy <akashasy53@gmail.com>
2022-05-17 17:53:39 +0530

    Updates

---
+++ precache-manifest.3030bc8e6837776e61acfb2239536e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f41bea9017204cc7dfc",
+    "url": "/reliving_miniproject/static/css/main.a70b2c6f.chunk.css"
+  },
+  {
+    "revision": "0f41bea9017204cc7dfc",
+    "url": "/reliving_miniproject/static/js/main.0f41bea9.chunk.js"
+  },
+  {
+    "revision": "f4436cdeb50cb18469bb",
+    "url": "/reliving_miniproject/static/js/1.f4436cde.chunk.js"
+  },
+  {
+    "revision": "fe1d4711ff2685731011",
+    "url": "/reliving_miniproject/static/js/runtime~main.fe1d4711.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/reliving_miniproject/static/media/memories.5c0c63fb.png"
+  },
+  {
+    "revision": "432717ea14353b0e3c588dfbb8a55731",
+    "url": "/reliving_miniproject/index.html"
+  }
+];
